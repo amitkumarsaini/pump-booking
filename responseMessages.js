2ea1831d275e4d8915c39cc3151f90e04ac6b6c6
amitkumarsaini <51169102+amitkumarsaini@users.noreply.github.com>
2020-09-20 22:11:33 +0530

    Add files via upload

---
+++ responseMessages.js
@@ -0,0 +1,18 @@
+/*  ©2020 Ozvid Technologies Pvt. Ltd. All Rights Reserved.Hosted by jiWebHosting.com */
+module.exports = {
+  USERNOTFOUND: "User not found.",
+  LOGINSUCCESSFULL: "Logged in successfully.",
+  PASSWORDNOTMATCH: "password is incorrect.",
+  NEWREGISTRATION: "New user registered",
+  REGISTRATIONFAILED: "Registration failed.",
+  NODATAFOUND: "No data found",
+  BLOCKED: "Sorry! your account is blocked.",
+  ERROR: "Some error occurred.",
+  UNIQUEERROR: (name) => {
+    return name + " already exists.";
+  },
+  UNAUTHORIZED: "Unauthorized , unable to do the action.",
+  DATAADDED: "Data added successfully.",
+  NOIMAGE: "No Image, Please Pass An Image ",
+  IMAGEUPLOADED: "Image uploaded successfully.",
+};
